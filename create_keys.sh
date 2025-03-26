4b06aac079f29be94f9df89390443251ad4480d8
Louis-Alexandre Duchesneau <la@duchesneau.dev>
2025-03-26 09:11:59 -0400

    using env bash, a request sudo access if needed

--- create_keys.sh
+++ create_keys.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 echoerr() { cat <<< "$@" 1>&2; }
 
 
--- create_usb.sh
+++ create_usb.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin//env bash
+
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
 echoerr() { cat <<< "$@" 1>&2; }
 devs=$(lsblk --noheadings --nodeps --output NAME,SIZE,TRAN,RM | sed 's/\s\s*/|/g' | grep "usb|1$")
 if [ -z $devs ]; then
@@ -13,24 +19,24 @@ select dev in $devs; do
         exit 2
     fi
     spec="start=2048 size=32768 type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B bootable attrs=RequiredPartition"
-    echo $spec | sfdisk -q --wipe always /dev/${dev} -X gpt
+    echo $spec | $SUDO sfdisk -q --wipe always /dev/${dev} -X gpt
     if [ $? -ne 0 ]; then
         echoerr sfdisk failed, needs sudo?
         exit 3
     fi
-    mkfs.fat /dev/${dev}1
+    $SUDO mkfs.fat /dev/${dev}1
     if [ $? -ne 0 ]; then
         echoerr mkfs failed
         exit 4
     fi
-    mount /dev/${dev}1 /mnt 
+    $SUDO mount /dev/${dev}1 /mnt 
     if [ $? -ne 0 ]; then
         echoerr mount failed
         exit 5
     fi
-    find keys -type f -name "*.BEK" -exec echo {} \; -exec cp {} /mnt \;
-    find keys -type f -name "*.lek" -exec echo {} \; -exec cp {} /mnt \;
-    umount /mnt
+    $SUDO find keys -type f -name "*.BEK" -exec echo {} \; -exec cp {} /mnt \;
+    $SUDO find keys -type f -name "*.lek" -exec echo {} \; -exec cp {} /mnt \;
+    $SUDO umount /mnt
     exit 0
 done
 
